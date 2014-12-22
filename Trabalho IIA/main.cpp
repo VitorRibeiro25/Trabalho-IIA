669bdbd31d1b74d0fa76e658248f2b036aa48a16
VitorRibeiro25 <vitor-ribeiro-1995@hotmail.com>
2014-12-22 14:41:51 +0000

    sa

--- Trabalho IIA/main.cpp
+++ Trabalho IIA/main.cpp
@@ -3,6 +3,17 @@
 
 using namespace std;
 
+//basicamente este trabalho temos de criar 3 funçoes 
+// 1. uma para o algoritmo de pesquisa local, neste caso eu sugeria em escolhermos o trepa-colinas
+// 2. algoritmo evolutivo, isto ainda nao sei o que é
+// 3. metodo hibrido que segundo o que aqui diz serve para combinar as abordagens anteriores
+
+
+// trepa colinas, algoritmo: algoritmo.cpp
+// isto ainda está uma confusao, mas o algoritmo do trepa colinas é mais ou menos isto
+
+
 void main(){
 	cout << "IIA" << endl;
 }
+
